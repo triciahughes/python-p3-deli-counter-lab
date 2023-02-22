2f0e1c52d3a94b147af8d1d4f0bf18696a18cab2
triciahughes <hughes.a.tricia@gmail.com>
2023-02-22 09:02:06 -0800

    passing one test

---
+++ lib/deli_counter.py
@@ -0,0 +1,19 @@
+katz_deli = []
+
+def line(position):
+    if len(katz_deli) == 0:
+        print("The line is currently empty.")
+    else:
+        line = len(katz_deli)
+        return print(f"The line is currently: {line}. {position}")
+
+line(["kyle"])
+
+def take_a_number(katz_deli, name):
+    katz_deli + name
+    print(f"Welcome, {name}. You are number")
+    pass
+
+def now_serving():
+    pass
+
